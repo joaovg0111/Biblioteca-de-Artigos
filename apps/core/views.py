cf0e508cc4895f261188cce26ee949b23682890a
Luis Maciel <luishenrique.8804@gmail.com>
2025-09-14 21:01:26 +0000

    fist view

--- apps/core/views.py
+++ apps/core/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    """
+    This view renders the main home page of the site.
+    """
+    return render(request, "index.html")
+
